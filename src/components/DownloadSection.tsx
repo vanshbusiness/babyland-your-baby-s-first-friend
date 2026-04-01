7f485fe5c1064ac823c0d0fa9a63af84c5c2a76e
Ragib Ali <66816874+ragibalikhan@users.noreply.github.com>
2026-04-02 02:27:40 +0530

    Update DownloadSection.tsx

--- src/components/DownloadSection.tsx
+++ src/components/DownloadSection.tsx
@@ -69,8 +69,12 @@ const DownloadSection = () => {
               className="bg-primary-foreground text-primary hover:bg-primary-foreground/90 shadow-soft hover:shadow-glow"
               asChild
             >
-              <a href="#" target="_blank" rel="noopener noreferrer">
-                <Download className="w-5 h-5" />
+              <a 
+                href="https://drive.google.com/uc?export=download&id=1zentH0QSmMRC9HHoD3B44RrLttqA1--l" 
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <Download className="w-5 h-5 mr-2" />
                 Download Babyland APK
               </a>
             </Button>
